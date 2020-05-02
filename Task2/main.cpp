4508f058ec3374609e6f7149bb492c4035ffa8e5
KamilTazet <38397573+KamilTazet@users.noreply.github.com>
2020-05-02 03:49:53 +0300

    Update main.cpp

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -2,10 +2,6 @@
 
 int main(int argc, char ** argv) {
     tinyxml2::XMLDocument file;
-    // char file_name[] = "/home/kamil/AB_Soft/Task2/test.xml";
-    // Company * cmpy = new Company(file_name);
-    // Company * cmpy = new Company(argv[1]);
-    // Invoker inv(file_name);
     Invoker inv(argv[1]);
     inv.listen();
 }
