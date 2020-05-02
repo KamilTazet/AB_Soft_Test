7a6504008e4c7bf6269f07c4452f8ec0fd63355f
KamilTazet <38397573+KamilTazet@users.noreply.github.com>
2020-05-02 19:55:46 +0300

    Add files via upload

--- Task2/company.cpp
+++ Task2/company.cpp
@@ -399,7 +399,9 @@ void History::save_command(Command * cmd) {
 void History::undo_last_command() {
     if(!history.empty()) {
         this->history.top()->undo();
+        Command * cmd = this->history.top();
         this->history.pop();
+        delete cmd;
     }
     else {
         std::cout << "Нет операций для отмены" << std::endl;
