0b136eb6820fbcab215f6be1181b0c18135c3770
KamilTazet <38397573+KamilTazet@users.noreply.github.com>
2020-05-13 06:35:57 +0300

    Добавлен класс Redo и реализации set_data
    Добавлен класс Redo, осуществляющий возврат отменённых изменений, добавлены реализации set_data() для классов конкретных команд, осуществляющий заполнение данныхи одновременную их проверку.

--- Task2/command_realisation.cpp
+++ Task2/command_realisation.cpp
@@ -1,14 +1,15 @@
 #include "command_realisation.h"
 
 
-DeleteDepartment::DeleteDepartment(Company * cmp, std::string name): Command(cmp) {
-    this->department_name = name;
-};
+DeleteDepartment::DeleteDepartment(Company * cmp): Command(cmp) {};
 
-bool DeleteDepartment::execute() {
-    Department * dlt_dp = new Department();
-    if(this->company->delete_department(this->department_name, dlt_dp)) {
-        deleted_department = *dlt_dp;
+
+bool DeleteDepartment::set_data() {
+    std::string dep_name;
+    std::cout << "Введите название удаляемого департамента: " << std::endl;
+    std::getline(std::cin, dep_name);
+    if(this->company->check_department(dep_name)) {
+        this->department_name = dep_name;
         return true;
     }
     else {
@@ -16,14 +17,52 @@ bool DeleteDepartment::execute() {
     }
 };
 
+bool DeleteDepartment::execute() {
+    Department * dlt_dp = new Department();
+    if(this->company->delete_department(this->department_name, dlt_dp)) {
+        deleted_department = *dlt_dp;
+        delete dlt_dp;
+        return true;
+    }
+    else {
+        delete dlt_dp;
+        return false;
+    }
+};
+
+
 void DeleteDepartment::undo() {
     this->company->add_department(deleted_department);
 };
 
+DeleteDepartment::~DeleteDepartment() {
 
-DeleteEmployee::DeleteEmployee(Company * cmp, std::string dp_name, std::string emp_name): Command(cmp) {
-    this->department_name = dp_name;
-    this->employee_name = emp_name;
+};
+
+
+DeleteEmployee::DeleteEmployee(Company * cmp): Command(cmp) {};
+
+bool DeleteEmployee::set_data() {
+    std::string dep_name;
+    std::string emp_name;
+    std::cout << "Введите: " << std::endl;
+    std::cout << "Название департамента удаляемого сотрудника: " << std::endl;
+    std::getline(std::cin, dep_name);
+    if(this->company->check_department(dep_name)) {
+        this->department_name = dep_name;
+        std::cout << "ФИО удаляемого сотрудника: " << std::endl;
+        std::getline(std::cin, emp_name);
+        if(this->company->check_employee(dep_name, emp_name)) {
+            this->employee_name = emp_name;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 bool DeleteEmployee::execute() {
@@ -43,10 +82,22 @@ void DeleteEmployee::undo() {
     this->company->add_employee(deleted_employee, department_name);
 };
 
+DeleteEmployee::~DeleteEmployee() {
 
-AddDepartment::AddDepartment(Company * cmp, std::string dp_name): Command(cmp) {
-    this->department_name = dp_name;
 };
+
+
+AddDepartment::AddDepartment(Company * cmp): Command(cmp) {};
+
+
+bool AddDepartment::set_data() {
+    std::string dep_name;
+    std::cout << "Введите название добавляемого департамента: " << std::endl;
+    std::getline(std::cin, dep_name);
+    this->department_name = dep_name;
+    return true;
+};
+
 bool AddDepartment::execute() {
     if(this->company->add_department(department_name)){
         return true;
@@ -59,10 +110,29 @@ void AddDepartment::undo() {
     this->company->delete_department(department_name);
 };
 
+AddDepartment::~AddDepartment() {
 
-AddEmployee::AddEmployee(Company * cmp, std::string dp_name, std::string emp_name): Command(cmp) {
-    this->department_name = dp_name;
-    this->employee_name = emp_name;
+};
+
+
+AddEmployee::AddEmployee(Company * cmp): Command(cmp) {};
+
+bool AddEmployee::set_data() {
+    std::string dep_name;
+    std::string emp_name;
+    std::cout << "Введите: " << std::endl;
+    std::cout << "Название департамента добавляемого сотрудника: " << std::endl;
+    std::getline(std::cin, dep_name);
+    if(this->company->check_department(dep_name)) {
+        this->department_name = dep_name;
+        std::cout << "ФИО добавляемого сотрудника: " << std::endl;
+        std::getline(std::cin, emp_name);
+        this->employee_name = emp_name;
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 bool AddEmployee::execute() {
@@ -78,10 +148,30 @@ void AddEmployee::undo() {
     this->company->delete_employee(department_name, employee_name);
 };
 
+AddEmployee::~AddEmployee() {
 
-ChangeDepartmentName::ChangeDepartmentName(Company * cmp, std::string old_name, std::string new_name): Command(cmp) {
-    this->old_department_name = old_name;
-    this->new_department_name = new_name;
+};
+
+
+ChangeDepartmentName::ChangeDepartmentName(Company * cmp): Command(cmp) {};
+
+
+bool ChangeDepartmentName::set_data() {
+    std::string old_dep_name;
+    std::string new_dep_name;
+    std::cout << "Введите: " << std::endl;
+    std::cout << "Название изменяемого департамента: " << std::endl;
+    std::getline(std::cin, old_dep_name);
+    if(this->company->check_department(old_dep_name)) {
+        this->old_department_name = old_dep_name;
+        std::cout << "Новое название департамента: " << std::endl;
+        std::getline(std::cin, new_dep_name);
+        this->new_department_name = new_dep_name;
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 bool ChangeDepartmentName::execute() {
@@ -92,12 +182,47 @@ void ChangeDepartmentName::undo() {
     this->company->change_department_name(new_department_name, old_department_name);
 };
 
+ChangeDepartmentName::~ChangeDepartmentName() {
 
-ChangeEmployeeAttribute::ChangeEmployeeAttribute(Company * cmp, std::string dep_name, std::string emp_name, std::string attr_name, std::string attr_value): Command(cmp) {
-    this->department_name = dep_name;
-    this->employee_name = emp_name;
-    this->attribute_name = attr_name;
-    this->new_attribute_value = attr_value;
+};
+
+
+ChangeEmployeeAttribute::ChangeEmployeeAttribute(Company * cmp): Command(cmp) {};
+
+bool ChangeEmployeeAttribute::set_data() {
+    std::string dep_name;
+    std::string emp_name;
+    std::string attr_name;
+    std::string attr_value;
+    std::cout << "Введите: " << std::endl;
+    std::cout << "Название департамента изменяемого сотрудника: " << std::endl;
+    std::getline(std::cin, dep_name);
+    if(this->company->check_department(dep_name)) {
+        this->department_name = dep_name;
+        std::cout << "ФИО изменяемого сотрудника: " << std::endl;
+        std::getline(std::cin, emp_name);
+        if(this->company->check_employee(dep_name, emp_name)) {
+            this->employee_name = emp_name;
+            std::cout << "Название изменяемого аттрибута: " << std::endl;
+            std::getline(std::cin, attr_name);
+            if(this->company->check_employee_attribute(dep_name, emp_name, attr_name)) {
+                this->attribute_name = attr_name;
+                std::cout << "Новое значение аттрибута: " << std::endl;
+                std::getline(std::cin, attr_value);
+                this->new_attribute_value = attr_value;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 bool ChangeEmployeeAttribute::execute() {
@@ -108,20 +233,50 @@ void ChangeEmployeeAttribute::undo() {
     this->company->change_employee_attribute(department_name, &employee_name, attribute_name, old_attribute_value, &new_attribute_value);
 };
 
+ChangeEmployeeAttribute::~ChangeEmployeeAttribute() {
 
-Save::Save(Company * cmp, char * file): Command(cmp) {
-    this->file_name = file;
 };
+
+
+Save::Save(Company * cmp): Command(cmp) {};
+
+
+bool Save::set_data() {
+    std::string save_name;
+    std::cout << "Введите имя для сохраняемого файла" << std::endl;
+    std::cout << "Расширение .xml будет добавлено автоматически" << std::endl;
+    std::getline(std::cin, save_name);
+    if(save_name.size() != 0) {
+        save_name = save_name + ".xml";
+        this->file_name = save_name;
+        return true;
+    }
+    else {
+        std::cout << "Введена пустая строка, сохранения не произведено" << std::endl;
+        return false;
+    }
+};
+
 bool Save::execute() {
-    company->save_doc(file_name);
+    char save_file_name[file_name.size()];
+    strcpy(save_file_name, this->file_name.c_str());
+    company->save_doc(save_file_name);
     return false;
 };
 void Save::undo() {
 
 };
 
+Save::~Save() {
+
+};
+
 
 Print::Print(Company * cmp): Command(cmp) {
+
+};
+
+bool Print::set_data() {
 
 };
 
@@ -134,6 +289,14 @@ void Print::undo() {
 
 };
 
+Print::~Print() {
+
+};
+
+
+bool Help::set_data() {
+
+};
 
 bool Help::execute() {
     std::cout << "Список команд:" << std::endl;
@@ -145,6 +308,7 @@ bool Help::execute() {
     std::cout << "change_employee_attribute - изменить значение аттрибутов сотрудника (фамилию, имя, отчество, должность и зарплату)" << std::endl;
     std::cout << "print - вывести отделы, сотрудников и их аттрибуты в терминал" << std::endl;
     std::cout << "undo - отмена последней команды" << std::endl;
+    std::cout << "redo - возврат последней отменённой команды" << std::endl;
     std::cout << "save - сохранить изменения" << std::endl;
     std::cout << "help - вывод списка команд" << std::endl;
     return false;
@@ -154,9 +318,17 @@ void Help::undo() {
 
 };
 
+Help::~Help() {
+
+};
+
 
 Undo::Undo(History * command_history) {
     this->history = command_history;
+};
+
+bool Undo::set_data() {
+
 };
 
 bool Undo::execute() {
@@ -165,5 +337,31 @@ bool Undo::execute() {
 };
 
 void Undo::undo() {
+
+};
+
+Undo::~Undo() {
+
+};
+
+
+Redo::Redo(History * command_history) {
+    this->history = command_history;
+};
+
+bool Redo::set_data() {
+
+};
+
+bool Redo::execute() {
+    this->history->redo_last_command();
+    return false;
+};
+
+void Redo::undo() {
+
+};
+
+Redo::~Redo() {
 
 };
--- Task2/command_realisation.h
+++ Task2/command_realisation.h
@@ -1,99 +1,129 @@
+#include <cstring>
 #include "history.h"
 
-class DeleteDepartment: public Command {
+class DeleteDepartment: public Command { //Класс команды удаления департамента
 private:
-    std::string department_name;
-    Department deleted_department;
+    std::string department_name; //Название удаляемого департамента
+    Department deleted_department; //Удалённый департамент. Нужен для восстановаления дпертамента в случае отмены удаления
 public:
-    DeleteDepartment(char file_name[], std::string name);
-    DeleteDepartment(Company * cmp, std::string name);
+    DeleteDepartment(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~DeleteDepartment();
 };
 
-class DeleteEmployee: public Command {
+class DeleteEmployee: public Command { //Класс команды удаления сотрудника
 private:
-    std::string department_name;
-    std::string employee_name;
-    Employee deleted_employee;
+    std::string department_name; //Название департамента
+    std::string employee_name; //ФИО удаляемого сотрудника
+    Employee deleted_employee; //Удалённый сотрудник. Нужен для восстановления сотрудника в случае отмены команды
 public:
-    DeleteEmployee(char file_name[], std::string dp_name, std::string emp_name);
-    DeleteEmployee(Company * cmp, std::string dp_name, std::string emp_name);
+    DeleteEmployee(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~DeleteEmployee();
 };
 
-class AddDepartment: public Command {
+class AddDepartment: public Command { //Класс команды добавления департамента
 private:
-    std::string department_name;
+    std::string department_name; //Нзвания добавляемого департамента
 public:
-    AddDepartment(Company * cmp, std::string dp_name);
+    AddDepartment(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~AddDepartment();
 };
 
-class AddEmployee: public Command{
+class AddEmployee: public Command{ //Класс команды добавления сотрудника
 private:
-    std::string department_name;
-    std::string employee_name;
+    std::string department_name; //Название департмента
+    std::string employee_name; //ФИО добавляемого сотрудника
 public:
-    AddEmployee(Company * cmp, std::string dp_name, std::string emp_name);
+    AddEmployee(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~AddEmployee();
 };
 
-class ChangeDepartmentName: public Command {
+class ChangeDepartmentName: public Command { //Класс команды изменения имени департамента
 private:
-    std::string old_department_name;
-    std::string new_department_name;
+    std::string old_department_name; //Изначальное название изменяемого и департамента
+    std::string new_department_name; //Новое название департамента
 public:
-    ChangeDepartmentName(Company * cmp, std::string old_name, std::string new_name);
+    ChangeDepartmentName(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~ChangeDepartmentName();
 };
 
-class ChangeEmployeeAttribute: public Command {
+class ChangeEmployeeAttribute: public Command { //Класс команды изменения значения аттрибута сотрудника
 private:
-    std::string department_name;
-    std::string employee_name;
-    std::string attribute_name;
-    std::string new_attribute_value;
-    std::string old_attribute_value;
+    std::string department_name; //Название департамента
+    std::string employee_name; //ФИО сотрудника
+    std::string attribute_name; //Название атрибута
+    std::string new_attribute_value; //Новое значение атрибута
+    std::string old_attribute_value; //Старое значение атрибута. Нужно для отмены команды
 public:
-    ChangeEmployeeAttribute(Company * cmp, std::string dep_name, std::string emp_name, std::string attr_name, std::string attr_value);
+    ChangeEmployeeAttribute(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~ChangeEmployeeAttribute();
 };
 
 
-class Save: public Command {
-    char * file_name;
+class Save: public Command { //Класс команды сохранения
+    std::string file_name; //Название файла сохранения
 public:
-    Save(Company * cmp, char * file);
+    Save(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~Save();
 };
 
 
-class Print: public Command {
+class Print: public Command { //Класс команды вывода состава компании
 public:
     Print(char file_name[]);
     Print(Company * cmp);
+    bool set_data();
     bool execute();
     void undo();
+    ~Print();
 };
 
-class Help: public Command {
+class Help: public Command { //Класс команды вывода списка команд с кратким пояснением
 public:
+    bool set_data();
     bool execute();
     void undo();
+    ~Help();
 };
 
-class Undo: public Command {
+class Undo: public Command { //Класс команды отмены действия
 private:
-    History * history;
+    History * history; //Указатель на объект истории изменений
 public:
     Undo(History * command_history);
+    bool set_data();
     bool execute();
     void undo();
+    ~Undo();
+};
+
+class Redo: public Command { //Класс команды возврата отменённого действия
+private:
+    History * history; //Указатель на объект истории изменений
+public:
+    Redo(History * command_history);
+    bool set_data();
+    bool execute();
+    void undo();
+    ~Redo();
 };
