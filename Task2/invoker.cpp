6e40164056cc849c584fbc7cc967a9c7b32ed279
KamilTazet <38397573+KamilTazet@users.noreply.github.com>
2020-05-13 07:09:32 +0300

    Изменено создание команд, добавлена обработка redo
    Изменено создание команд, добавлена обработка команды возврата изменений redo

--- Task2/invoker.cpp
+++ Task2/invoker.cpp
@@ -7,97 +7,99 @@ Invoker::Invoker(char * file_name): company(file_name) {
 int Invoker::listen() {
     std::string command_line;
     for(;;) {
-        Command * command;
+        std::unique_ptr<Command> command;
         std::cout << "Введите команду (для завершения введите quit): " << std::endl;
         std::getline(std::cin, command_line);
         if(command_line == "delete_department") {
-            std::string department_name;
-            std::cout << "Введите название удаляемого департамента: " << std::endl;
-            std::getline(std::cin, department_name);
-            DeleteDepartment * del_dep = new DeleteDepartment(&(this->company), department_name);
-            command = del_dep;
+            std::unique_ptr<Command> del_dep(new DeleteDepartment(&(this->company)));
+            if(del_dep->set_data()) {
+                command = std::move(del_dep);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "delete_employee") {
-            std::string department_name;
-            std::string employee_name;
-            std::cout << "Введите: " << std::endl;
-            std::cout << "Название департамента удаляемого сотрудника: " << std::endl;
-            std::getline(std::cin, department_name);
-            std::cout << "ФИО удаляемого сотрудника: " << std::endl;
-            std::getline(std::cin, employee_name);
-            DeleteEmployee * del_emp = new DeleteEmployee(&(this->company), department_name, employee_name);
-            command = del_emp;
+            std::unique_ptr<Command> del_emp(new DeleteEmployee(&(this->company)));
+            if(del_emp->set_data()) {
+                command = std::move(del_emp);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "add_department") {
-            std::string department_name;
-            std::cout << "Введите название добавляемого департамента: " << std::endl;
-            std::getline(std::cin, department_name);
-            AddDepartment * add_dep = new AddDepartment(&(this->company), department_name);
-            command = add_dep;
+            std::unique_ptr<Command> add_dep(new AddDepartment(&(this->company)));
+            if(add_dep->set_data()) {
+                command = std::move(add_dep);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "add_employee") {
-            std::string department_name;
-            std::string employee_name;
-            std::cout << "Введите: " << std::endl;
-            std::cout << "Название департамента добавляемого сотрудника: " << std::endl;
-            std::getline(std::cin, department_name);
-            std::cout << "ФИО добавляемого сотрудника: " << std::endl;
-            std::getline(std::cin, employee_name);
-            AddEmployee * add_emp = new AddEmployee(&(this->company), department_name, employee_name);
-            command = add_emp;
+            std::unique_ptr<Command> add_emp(new AddEmployee(&(this->company)));
+            if(add_emp->set_data()) {
+                command = std::move(add_emp);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "change_department_name") {
-            std::string old_department_name;
-            std::string new_department_name;
-            std::cout << "Введите: " << std::endl;
-            std::cout << "Название изменяемого департамента: " << std::endl;
-            std::getline(std::cin, old_department_name);
-            std::cout << "Новое название департамента: " << std::endl;
-            std::getline(std::cin, new_department_name);
-            ChangeDepartmentName * chng_dep = new ChangeDepartmentName(&(this->company), old_department_name, new_department_name);
-            command = chng_dep;
+            std::unique_ptr<Command> chng_dep(new ChangeDepartmentName(&(this->company)));
+            if(chng_dep->set_data()) {
+                command = std::move(chng_dep);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "change_employee_attribute") {
-            std::string department_name;
-            std::string employee_name;
-            std::string attribute_name;
-            std::string attribute_value;
-            std::cout << "Введите: " << std::endl;
-            std::cout << "Название департамента изменяемого сотрудника: " << std::endl;
-            std::getline(std::cin, department_name);
-            std::cout << "ФИО изменяемого сотрудника: " << std::endl;
-            std::getline(std::cin, employee_name);
-            std::cout << "Название изменяемого аттрибута: " << std::endl;
-            std::getline(std::cin, attribute_name);
-            std::cout << "Новое значение аттрибута: " << std::endl;
-            std::getline(std::cin, attribute_value);
-            ChangeEmployeeAttribute * chng_emp = new ChangeEmployeeAttribute(&(this->company), department_name, employee_name, attribute_name, attribute_value);
-            command = chng_emp;
+            std::unique_ptr<Command> chng_emp(new ChangeEmployeeAttribute(&(this->company)));
+            if(chng_emp->set_data()) {
+                command = std::move(chng_emp);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "save") {
-            Save * save_com = new Save(&(this->company), this->file_name);
-            command = save_com;
+            std::unique_ptr<Command> save_com(new Save(&(this->company)));
+            if(save_com->set_data()) {
+                command = std::move(save_com);
+            }
+            else {
+                continue;
+            }
         }
         else if(command_line == "print") {
-            Print * prnt_com = new Print(&(this->company));
-            command = prnt_com;
+            // Print * prnt_com = new Print(&(this->company));
+            std::unique_ptr<Command> prnt_com(new Print(&(this->company)));
+            // command = prnt_com;
+            command = std::move(prnt_com);
         }
         else if(command_line == "undo") {
-            Undo * undo_com = new Undo(&(this->history));
-            command = undo_com;
+            std::unique_ptr<Command> undo_com(new Undo(&(this->history)));
+            command = std::move(undo_com);
+        }
+        else if(command_line == "redo") {
+            std::unique_ptr<Command> redo_com(new Redo(&(this->history)));
+            command = std::move(redo_com);
         }
         else if(command_line == "help") {
-            Help * help_com = new Help();
-            command = help_com;
+            std::unique_ptr<Command> help_com(new Help());
+            command = std::move(help_com);
         }
         else if(command_line == "quit"){
             return 0;
         }
         else {
             std::cout << "Неизвестная команда" << std::endl;
+            continue;
         }
-        if(command->execute()) {
-            history.save_command(command);
+        if(command->execute()) { //В случае успешного выполнения команды она добваляеться в историю изменений
+            history.save_command(std::move(command));
         }
     }
 };
--- Task2/invoker.h
+++ Task2/invoker.h
@@ -5,11 +5,10 @@
 
 class Invoker {
 private:
-    History history;
-    Company company;
-    // std::string file_name;
-    char * file_name;
+    History history; //Объект истории изменений
+    Company company; //Объект компании
+    char * file_name; //Название выбранного для считывания XML-файла
 public:
     Invoker(char * file_name);
-    int listen();
+    int listen(); //Метод считывания введёных команд и создания соответсвующих объектов команд 
 };
